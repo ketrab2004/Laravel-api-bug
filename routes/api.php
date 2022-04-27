b92bd64c7d9597d2d8314885ca7ee015fc989991
Bartek O <bartek@fos.nl>
2022-04-27 19:45:01 +0200

    Added the `ability` middleware to both the api and web user routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(["auth:sanctum", "ability:read-profile,user:read-profile"])->get("/user", function (Request $request) {
     return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
+Route::middleware(["auth:sanctum", "ability:read-profile,user:read-profile"])->get("/user", function (Request $request) {
     return $request->user();
 });
 
