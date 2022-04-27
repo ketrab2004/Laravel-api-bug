dbc94043e7fbb68fb469e1451adbbd3bf03ed329
Bartek O <bartek@fos.nl>
2022-04-27 19:05:04 +0200

    Made a copy of the `/api/user` route and added it to web routes
    `/user`

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
+    return $request->user();
 });
 
 Route::controller(LoginController::class)->group(function() {
