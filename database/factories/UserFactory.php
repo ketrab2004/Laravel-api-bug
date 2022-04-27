1cb683eea6ecd725724532495a40b46a3ef1e4c8
Bartek O <bartek@fos.nl>
2022-04-27 19:33:11 +0200

    Made the UserFactory give every created user a token for reading their profile

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -24,6 +25,16 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
+    }
+
+    /**
+     * Configure special attributes for this factory
+     */
+    public function configure()
+    {
+        return $this->afterCreating(function(User $user) {
+            $user->createToken("user", ['*', "user:read-profile"]); // add a token for every user
+        });
     }
 
     /**
