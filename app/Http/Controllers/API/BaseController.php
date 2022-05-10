2c1d878a21485340acd07ed985e5e0337307ae2b
Bartek O <bartek@fos.nl>
2022-05-10 19:06:27 +0200

    Made data in handleResponse() in API Basecontroller be optional
    so you could only return success = true

--- app/Http/Controllers/API/BaseController.php
+++ app/Http/Controllers/API/BaseController.php
@@ -10,16 +10,18 @@ class BaseController extends Controller
     /**
      * Handle a json request.
      *
-     * @param mixed $result what to return
+     * @param ?mixed $data to return
      * @param ?string $msg optional message to give
      * @return JsonResponse json object response
      */
-    public function handleResponse(mixed $result, ?string $msg): JsonResponse
+    public function handleResponse(mixed $data, ?string $msg): JsonResponse
     {
         $result = [
-            "success" => true,
-            "data" => $result
+            "success" => true
         ];
+
+        // set data if provided
+        if (isset($data)) $result["data"] = $data;
 
         // set message if provided
         if (isset($msg)) $result["message"] = $msg;
@@ -31,7 +33,7 @@ class BaseController extends Controller
      * Handle a json Errored request.
      *
      * @param string $error error message
-     * @param array $errorMsgs array of php error message?
+     * @param ?array $errorMsgs array of messages (for example, validation errors)
      * @param ?int $code type of response
      * @return JsonResponse json object response
      */
